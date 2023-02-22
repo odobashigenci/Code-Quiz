5d1fe21606ac73e83edff67ae02ae39971eccb24
odobashigenci <121208981+odobashigenci@users.noreply.github.com>
2023-02-22 14:21:51 -0500

    updating final code

--- Assets/js/highscore.js
+++ Assets/js/highscore.js
@@ -1,10 +1,25 @@
+
+var highScore = JSON.parse(localStorage.getItem("score")) || [];
+
+// Event listener to Reaset Score button, to clear scores.
 var reset = document.querySelector("#resetScore");
-
-
-// Event listener to clear scores 
 reset.addEventListener("click", function (){ 
     localStorage.clear();
     location.reload();
     console.log("clicked");
   })
-  
+
+
+  // Render score on screen.
+
+  var scoreTable = document.getElementById("scoreTable");
+  highScore.forEach( function (item){
+    console.log(item)
+    var createLi = document.createElement("li");
+    createLi.innerHTML = 'Name: ' + item.initials + " - " + ' Score: ' + item.timeLeft
+    console.log(createLi)
+    scoreTable.append(createLi)
+  })
+
+ 
+
